1f288fb82b69979ac2e3093cdf66a06143a16942
gesoripro <formelodyne@gmail.com>
2026-02-04 18:07:07 +0900

    Fix: Remove duplicate Firebase code from app.js

--- js/app.js
+++ js/app.js
@@ -2,51 +2,8 @@
 // Yacht Dice - 앱 초기화
 // =====================================================
 
-// Firebase 설정 (firebase-config.js에서 가져옴)
-const firebaseConfig = {
-    apiKey: "AIzaSyBSkizTO7o70NqUz90GLkYiwj9XGMKTCQU",
-    authDomain: "rockscissorpaper-3b569.firebaseapp.com",
-    databaseURL: "https://rockscissorpaper-3b569-default-rtdb.firebaseio.com",
-    projectId: "rockscissorpaper-3b569",
-    storageBucket: "rockscissorpaper-3b569.firebasestorage.app",
-    messagingSenderId: "55192953735",
-    appId: "1:55192953735:web:6cba6b4d46da0dc1c0a922",
-    measurementId: "G-S3SF1Y9HR9"
-};
-
-// 전역 Firebase 참조
-let app, auth, database;
-
-// Firebase 초기화
-function initializeFirebase() {
-    if (typeof firebase !== 'undefined') {
-        app = firebase.initializeApp(firebaseConfig);
-        auth = firebase.auth();
-        database = firebase.database();
-        return signInAnonymously();
-    } else {
-        console.error('Firebase SDK가 로드되지 않았습니다.');
-        return Promise.reject(new Error('Firebase SDK not loaded'));
-    }
-}
-
-// 익명 로그인
-function signInAnonymously() {
-    return auth.signInAnonymously()
-        .then((userCredential) => {
-            console.log('익명 로그인 성공:', userCredential.user.uid);
-            return userCredential.user;
-        })
-        .catch((error) => {
-            console.error('익명 로그인 실패:', error);
-            throw error;
-        });
-}
-
-// 현재 사용자 ID
-function getCurrentUserId() {
-    return auth?.currentUser?.uid || null;
-}
+// firebase-config.js에서 Firebase 설정을 가져옴
+// app, auth, database 변수는 firebase-config.js에서 전역으로 선언됨
 
 // 앱 초기화
 async function initApp() {
